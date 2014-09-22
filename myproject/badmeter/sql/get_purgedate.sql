a3a6426645f0b4a7cb5f28fb436a906741ad7c9e
Cydric Lopez <CydricLopez@gmail.com>
2014-09-22 00:19:20 -0700

    Updated comments to badmeter/sql/get_purgedate.sql

--- myproject/badmeter/sql/get_purgedate.sql
+++ myproject/badmeter/sql/get_purgedate.sql
@@ -29,8 +29,8 @@ DECLARE
     t_end timestamp;
     t_count int;
     t_oneday interval := interval '1 day';
-    t_interval interval;    -- := interval '10 days';
-    t_quota int;            -- := 100;
+    t_interval interval;
+    t_quota int;
 BEGIN
     -- Get application configuration from central location
     -- in get_configuration().
@@ -51,10 +51,10 @@ BEGIN
 
     t_start := date_trunc('day', t_start);
 
-    -- End date is 10 days from start date;
+    -- End date is interval_days from start date;
     t_end := t_start + t_interval;
 
-    -- If topic made it beyond first 10-day period tweak start & end dates.
+    -- If topic made it beyond first interval_days period tweak start & end dates.
     IF t_end < t_now THEN
         t_end := date_trunc('day', t_now);
         t_start := t_end - t_interval;
@@ -64,7 +64,7 @@ BEGIN
         RAISE NOTICE 't_start=%',t_start;
         RAISE NOTICE 't_end=%',t_end;
 
-        -- Count the number of votes during the 10-day period.
+        -- Count the number of votes during the interval_days period.
         SELECT count(*)
             INTO t_count
             FROM badmeter_vote
@@ -78,13 +78,11 @@ BEGIN
         -- If there are not enough votes exit & return the end date.
         EXIT WHEN (t_count < t_quota);
 
-        -- Try again on the next 10-day period.
+        -- Try again on the next interval_days period.
         t_end := t_end + t_oneday;
         t_start := t_end - t_interval;
     END LOOP;
 
-    -- ~ purge_date := substring(t_end::text for 10);
-    -- ~ purge_date := to_char(t_end, 'FMMonth DD, YYYY HH:MI:SS');
     purge_date := to_char(t_end, 'FMMon. DD, YYYY');
     vote_needed := (t_quota - t_count)::text;
 END;
